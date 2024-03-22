1d0463d2939ff39a701e5cdc5336bba33c746ada
Alejandro Sánchez <alejandroszg@gmail.com>
2024-03-22 15:38:12 -0300

    feat: creating a social-link web application with Angular and Tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SocialComponent } from './components/social/social.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: SocialComponent
+    }
+];
